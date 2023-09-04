652b194fe1ea277445647f7b357743bc5b56fca3
Warren MacEvoy <wmacevoy@gmail.com>
2023-09-04 14:49:39 -0600

    cleaned tests

--- notes-ci/api/api.test.js
+++ notes-ci/api/api.test.js
@@ -1,77 +1,65 @@
 const request = require('supertest');
 const { db } = require('./db');
-const { app , start } = require('./app');
+const { app, start } = require('./app');
 
-const admin = { 'user_id' : 'alex', 'password' : '@bc!23' };
-const user = { 'user_id' : 'jordan', 'password' : 'abc123' };
-const nobody = { 'user_id' : 'murphy', 'password' : 'yes' };
+const admin = { 'user_id': 'alex', 'password': '@bc!23' };
+const user = { 'user_id': 'jordan', 'password': 'abc123' };
+const nobody = { 'user_id': 'murphy', 'password': 'yes' };
 
 const quiet = true;
 
 const tokenFor = async (user) => {
-    const payload = { 'user_id' : user.user_id, 'password' : user.password };
-    const response = await request(app)
-	  .post(`/api/v1/login`)
-	  .send(payload)
-	  .set('Accept', 'application/json')
-	  .expect('Content-Type', /json/)
-	  .expect(200);
-    expect(response.body).toHaveProperty('token');    
-    return response.body.token;
+	const payload = { 'user_id': user.user_id, 'password': user.password };
+	const response = await request(app)
+		.post(`/api/v1/login`)
+		.send(payload)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.expect(200);
+	expect(response.body).toHaveProperty('token');
+	return response.body.token;
 }
 
 describe('login', () => {
-    it('works for admin', async () => {
-	await db({'reset':true, quiet });
-	await tokenFor(admin);
-    });
-    it('works for user', async () => {
-	await db({'reset':true, quiet });
-	await tokenFor(user);
-    });
-    it('fails for nobody',async () => {
-	await db({'reset':true, quiet });
-	await db({'reset':true, quiet });
-	const payload = { 'user_id' : nobody.user_id, 'password' : nobody.password };
-	const response = await request(app)
-	      .post(`/api/v1/login`)
-	      .send(payload)
-	      .expect(401);
-    });
+	beforeAll(async () => await db({ 'reset': true, quiet }));
+	it('works for admin', async () => await tokenFor(admin));
+	it('works for user', async () => await tokenFor(user));
+	it('fails for nobody', async () => {
+		const payload = { 'user_id': nobody.user_id, 'password': nobody.password };
+		const response = await request(app)
+			.post(`/api/v1/login`)
+			.send(payload)
+			.expect(401);
+	});
 });
 
-describe('notes', () => {
-    it('works for admin', async () => {
-	await db({'reset':true, quiet });
-	const token = await tokenFor(admin);
-	const response = await request(app)
-	      .get(`/api/v1/notes`)
-	      .set('Authorization', `Bearer ${token}`)
-	      .set('Accept', 'application/json')
-	      .expect('Content-Type', /json/)
-	      .expect(200);
-
-	notes = response.body;
-	expect(notes[0].content).toBe('this is alex');
-    });
-    it('works for jordan', async () => {
-	await db({'reset':true, quiet });
+const notesFor = async (user) => {
 	const token = await tokenFor(user);
 	const response = await request(app)
-	      .get(`/api/v1/notes`)
-	      .set('Authorization', `Bearer ${token}`)
-	      .set('Accept', 'application/json')
-	      .expect('Content-Type', /json/)
-	      .expect(200);
-
+		.get(`/api/v1/notes`)
+		.set('Authorization', `Bearer ${token}`)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.expect(200);
 	notes = response.body;
-	expect(notes[0].content).toBe('this is jordan');
-    });
-    it('fails without authorization', async () => {
-	await db({'reset':true, quiet });
-	const response = await request(app)
-	      .get(`/api/v1/notes`)
-	      .set('Accept', 'application/json')
-	      .expect(403);
-    });
+	expect(notes).toEqual(expect.arrayContaining([expect.objectContaining({ content: expect.anything() })]));
+	return notes;
+};
+
+describe('notes', () => {
+	beforeEach(async () => await db({ 'reset': true, quiet }));
+	it('fails without authorization', async () => {
+		const response = await request(app)
+			.get(`/api/v1/notes`)
+			.set('Accept', 'application/json')
+			.expect(403);
+	});
+	it('works for admin', async () => {
+		const notes = await notesFor(admin);
+		expect(notes[0].content).toBe('this is alex');
+	});
+	it('works for user', async () => {
+		const notes = await notesFor(user);
+		expect(notes[0].content).toBe('this is jordan');
+	});
 });
--- notes-ci/api/db.js
+++ notes-ci/api/db.js
@@ -3,21 +3,20 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
-const config = async (pool,options) => {
+const config = async (pool, options) => {
     const reset = options?.reset ?? false;
     const quiet = options?.quiet ?? false;
-    console.log(`db config ${JSON.stringify(options)} reset ${reset} quiet ${quiet}`);
     try {
-	if (reset) {
-	    if (!quiet) {
-		console.log(`dropping tables`);
-	    }
+        if (reset) {
+            if (!quiet) {
+                console.log(`dropping tables`);
+            }
             await pool.query(`DROP TABLE IF EXISTS notes`);
-	    await pool.query(`DROP TABLE IF EXISTS user_roles`);
-	    await pool.query(`DROP TABLE IF EXISTS users`);
+            await pool.query(`DROP TABLE IF EXISTS user_roles`);
+            await pool.query(`DROP TABLE IF EXISTS users`);
             await pool.query(`DROP TABLE IF EXISTS roles`);
-	}
-	
+        }
+
         await pool.query(`
           CREATE TABLE IF NOT EXISTS roles (
             id VARCHAR PRIMARY KEY
@@ -48,25 +47,25 @@ const config = async (pool,options) => {
       );
     `);
 
-	const alex = { 'id': 'alex', 'password': '@bc!23', 'roles': ['admin','user'], notes : [{content:'this is alex'}] };
-	const jordan = {'id':'jordan','password':'abc123','roles':['user'], notes : [{content:'this is jordan'}] };
-	
-	for (const user of [alex,jordan]) {
-	    const jwt_secret = crypto.randomBytes(16).toString('hex');
-	    const hashedPassword = await bcrypt.hash(user.password, parseInt(process.env.BCRYPT_ROUNDS));
-	    await pool.query('INSERT INTO users (id, jwt_secret, hashed_password) VALUES ($1, $2, $3) ON CONFLICT(id) DO UPDATE SET jwt_secret = EXCLUDED.jwt_secret, hashed_password = EXCLUDED.hashed_password', [user.id, jwt_secret, hashedPassword]);
-	    await pool.query('DELETE FROM user_roles WHERE user_id = $1',[user.id]);
-	    for (const role of user.roles) {
-		await pool.query('INSERT INTO roles (id) VALUES ($1) ON CONFLICT DO NOTHING', [role]);
-		await pool.query('INSERT INTO user_roles (user_id,role_id) VALUES ($1,$2) ON CONFLICT DO NOTHING',[user.id,role]);
-	    }
-	    for (const note of user.notes) {
-		await pool.query('INSERT INTO notes (user_id, content) VALUES ($1, $2)', [user.id, note.content]);
-	    }
-	    if (!quiet) {
-		console.log(`${user.id} user created with password **** with roles ${JSON.stringify(user.roles)}`);
-	    }
-	}
+        const alex = { 'id': 'alex', 'password': '@bc!23', 'roles': ['admin', 'user'], notes: [{ content: 'this is alex' }] };
+        const jordan = { 'id': 'jordan', 'password': 'abc123', 'roles': ['user'], notes: [{ content: 'this is jordan' }] };
+
+        for (const user of [alex, jordan]) {
+            const jwt_secret = crypto.randomBytes(16).toString('hex');
+            const hashedPassword = await bcrypt.hash(user.password, parseInt(process.env.BCRYPT_ROUNDS));
+            await pool.query('INSERT INTO users (id, jwt_secret, hashed_password) VALUES ($1, $2, $3) ON CONFLICT(id) DO UPDATE SET jwt_secret = EXCLUDED.jwt_secret, hashed_password = EXCLUDED.hashed_password', [user.id, jwt_secret, hashedPassword]);
+            await pool.query('DELETE FROM user_roles WHERE user_id = $1', [user.id]);
+            for (const role of user.roles) {
+                await pool.query('INSERT INTO roles (id) VALUES ($1) ON CONFLICT DO NOTHING', [role]);
+                await pool.query('INSERT INTO user_roles (user_id,role_id) VALUES ($1,$2) ON CONFLICT DO NOTHING', [user.id, role]);
+            }
+            for (const note of user.notes) {
+                await pool.query('INSERT INTO notes (user_id, content) VALUES ($1, $2)', [user.id, note.content]);
+            }
+            if (!quiet) {
+                console.log(`${user.id} user created with password **** with roles ${JSON.stringify(user.roles)}`);
+            }
+        }
     } catch (err) {
         console.error(`Database initialization error: ${err.message}`);
     }
@@ -77,16 +76,16 @@ let pool = null;
 
 const db = async (options) => {
     if (pool === null) {
-	pool = new Pool({
-	    host: `notes-${process.env.NODE_ENV}-db`,
-	    port: process.env.PGPORT,
-	    database: process.env.POSTGRES_DB,
-	    user: process.env.POSTGRES_USER,
-	    password: process.env.POSTGRES_PASSWORD
-	});
-	await config(pool,options);
+        pool = new Pool({
+            host: `notes-${process.env.NODE_ENV}-db`,
+            port: process.env.PGPORT,
+            database: process.env.POSTGRES_DB,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD
+        });
+        await config(pool, options);
     } else if (options !== undefined) {
-	await config(pool,options);
+        await config(pool, options);
     }
     return pool;
 }
