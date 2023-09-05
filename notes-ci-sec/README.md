3354a826f98b8e88e17f9c34db9aa348cd03df18
Warren MacEvoy <wmacevoy@gmail.com>
2023-09-05 11:42:43 -0600

    secure commit

---
+++ notes-ci-sec/README.md
@@ -0,0 +1,39 @@
+# Notes
+
+This is a very simple app to illustrate the idea of a REST api with authorizations and permissions.  It does not have
+anything like full functionality, just enough to illustrate the mechanics of such a service.
+
+## Up/Down/Reset
+To start, use (in this directory after starting docker)
+
+```bash
+make all
+```
+
+To shut down, use
+
+```bash
+make down
+```
+
+If you want to reset the postgress database, you can re-initilize this by erasing the volume mount as well,
+
+```
+make clean
+```
+
+This way the next time the service starts it will re-create the tables.  Look at `api/db-init.js` for the initialization code.
+
+When running, you should be able to see the api (see `api/routes.js` for the routes) as http://localhost:3000/api/v1/login etc.  There are some very basic web pages served up from the 'api/static' folder so http://localhost:3000/login.html exercises the login api, etc.
+
+The development and test environments supports hot-reload so changes trigger a server / test re-run.
+
+The basic infrastructure is as follows:
+
+- The postgress database maintains these tables
+-- users (id, jwt_secret, hashed_password)
+-- roles (id) with `admin` and `user`
+-- user_roles (user_id,role_id)
+-- notes(user_id,content)
+
+the `api/config` has configuration and test data.  This is only loaded to the repo in an encrypted state using git-crypt
---
+++ notes-ci-sec/api/db.js
@@ -0,0 +1,103 @@
+const { Pool } = require('pg');
+
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+
+const config = async (pool, options) => {
+    const reset = options?.reset ?? false;
+    const quiet = options?.quiet ?? false;
+    try {
+
+        if (reset) {
+            if (!quiet) {
+                console.log(`dropping tables`);
+            }
+            await pool.query(`DROP TABLE IF EXISTS notes`);
+            await pool.query(`DROP TABLE IF EXISTS user_roles`);
+            await pool.query(`DROP TABLE IF EXISTS users`);
+            await pool.query(`DROP TABLE IF EXISTS roles`);
+        }
+
+        await pool.query(`
+          CREATE TABLE roles (
+            id VARCHAR PRIMARY KEY
+          );
+    `);
+	    
+        await pool.query(`
+          CREATE TABLE IF NOT EXISTS users (
+            id VARCHAR PRIMARY KEY,
+            jwt_secret VARCHAR NOT NULL,
+            hashed_password VARCHAR NOT NULL
+          );
+    `);
+
+        await pool.query(`
+          CREATE TABLE IF NOT EXISTS user_roles (
+            user_id VARCHAR REFERENCES users(id),
+            role_id VARCHAR REFERENCES roles(id),
+            PRIMARY KEY (user_id, role_id)
+          );
+    `);
+
+        await pool.query(`
+      CREATE TABLE IF NOT EXISTS notes (
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR REFERENCES users(id),
+        content TEXT
+      );
+    `);
+
+	const dataFile=`./private/data-${process.env.NODE_ENV}.json`;
+	const data=JSON.parse(await readFile(dataFile, 'utf8'));
+
+	for (const role of data.roles) {
+            await pool.query('INSERT INTO roles (id) VALUES ($1) ON CONFLICT DO NOTHING', [role]);
+	}
+
+        for (const user of data.users) {
+            const jwt_secret = crypto.randomBytes(16).toString('hex');
+            const hashedPassword = await bcrypt.hash(user.password, parseInt(process.env.BCRYPT_ROUNDS));
+            await pool.query('INSERT INTO users (id, jwt_secret, hashed_password) VALUES ($1, $2, $3) ON CONFLICT(id) DO UPDATE SET jwt_secret = EXCLUDED.jwt_secret, hashed_password = EXCLUDED.hashed_password', [user.id, jwt_secret, hashedPassword]);
+            await pool.query('DELETE FROM user_roles WHERE user_id = $1', [user.id]);
+            for (const role of user.roles) {
+                await pool.query('INSERT INTO user_roles (user_id,role_id) VALUES ($1,$2) ON CONFLICT DO NOTHING', [user.id, role]);
+            }
+	    if (!(await pool.query('SELECT COUNT(*) FROM notes WHERE user_id = $1',[user.id])).rows[0].count < user.notes.length) {
+		for (const note of user.notes) {
+                    await pool.query('INSERT INTO notes (user_id, content) VALUES ($1, $2)', [user.id, note.content]);
+		}
+	    }
+	    if (!quiet) {
+                console.log(`${user.id} user created with password **** with roles ${JSON.stringify(user.roles)}`);
+            }
+        }
+    } catch (err) {
+        console.error(`Database initialization error: ${err.message}`);
+    }
+};
+
+
+let pool = null;
+
+const db = async (options) => {
+    if (pool === null) {
+        pool = new Pool({
+            host: `notes-${process.env.NODE_ENV}-db`,
+            port: process.env.PGPORT,
+            database: process.env.POSTGRES_DB,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD
+        });
+        await config(pool, options);
+    } else if (options !== undefined) {
+        await config(pool, options);
+    }
+    return pool;
+}
+
+
+module.exports = { db };
