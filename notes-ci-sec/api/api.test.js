e6b4ef72ac7e0f5e9bed5053b703098aff051b2b
Warren MacEvoy <wmacevoy@gmail.com>
2023-09-06 13:09:22 -0600

    passing tests

--- notes-ci-sec/api/api.test.js
+++ notes-ci-sec/api/api.test.js
@@ -1,15 +1,14 @@
 const request = require('supertest');
 const { db } = require('./db');
 const { app, start } = require('./app');
+const { Data } = require('./data');
 
-const admin = { 'user_id': 'alex', 'password': '@bc!23' };
-const user = { 'user_id': 'jordan', 'password': 'abc123' };
-const nobody = { 'user_id': 'murphy', 'password': 'yes' };
+const data = new Data();
 
 const quiet = true;
 
 const tokenFor = async (user) => {
-	const payload = { 'user_id': user.user_id, 'password': user.password };
+	const payload = { 'user_id': user.id, 'password': user.password };
 	const response = await request(app)
 		.post(`/api/v1/login`)
 		.send(payload)
@@ -21,16 +20,31 @@ const tokenFor = async (user) => {
 }
 
 describe('login', () => {
-	beforeAll(async () => await db({ 'reset': true, quiet }));
-	it('works for admin', async () => await tokenFor(admin));
-	it('works for user', async () => await tokenFor(user));
-	it('fails for nobody', async () => {
-		const payload = { 'user_id': nobody.user_id, 'password': nobody.password };
-		const response = await request(app)
-			.post(`/api/v1/login`)
-			.send(payload)
-			.expect(401);
-	});
+    beforeAll(async () => await db({ 'reset': true, quiet }));
+    it('works for admins', async () => {
+	for (const admin of await data.getAdmins()) {
+	  await tokenFor(admin);
+	}
+    });
+    it('works for users', async () => {
+	for (const user of await data.getUsers()) {
+	  await tokenFor(user);
+	}
+    });
+    it('works for nobodies', async () => {
+	for (const nobody of await data.getNobodies()) {
+	  await tokenFor(nobody);
+	}
+    });
+    if ('fails for bad passwords',async () => {
+	for (const user of await data.getAllUsers()) {
+	    const payload = { 'user_id': user.id, 'password': `not ${user.password}` };
+	    const response = await request(app)
+		  .post(`/api/v1/login`)
+		  .send(payload)
+		  .expect(401);
+	}
+    });
 });
 
 const notesFor = async (user) => {
@@ -47,19 +61,37 @@ const notesFor = async (user) => {
 };
 
 describe('notes', () => {
-	beforeEach(async () => await db({ 'reset': true, quiet }));
-	it('fails without authorization', async () => {
-		const response = await request(app)
-			.get(`/api/v1/notes`)
-			.set('Accept', 'application/json')
-			.expect(403);
-	});
-	it('works for admin', async () => {
-		const notes = await notesFor(admin);
-		expect(notes[0].content).toBe('this is alex');
-	});
-	it('works for user', async () => {
-		const notes = await notesFor(user);
-		expect(notes[0].content).toBe('this is jordan');
-	});
+    beforeEach(async () => await db({ 'reset': true, quiet }));
+    it('fails without authorization', async () => {
+	const response = await request(app)
+	      .get(`/api/v1/notes`)
+	      .set('Accept', 'application/json')
+	      .expect(403);
+    });
+    it('works for admins', async () => {
+	for (const admin of await data.getAdmins()) {	
+	    const notes = await notesFor(admin);
+	    for (note of admin.notes) {
+		expect(notes).toEqual(expect.arrayContaining([expect.objectContaining({ content: note.content })]));		
+	    }
+	}
+    });
+    it('works for users', async () => {
+	for (const user of await data.getUsers()) {	
+	    const notes = await notesFor(user);
+	    for (note of user.notes) {
+		expect(notes).toEqual(expect.arrayContaining([expect.objectContaining({ content: note.content })]));
+	    }
+	}
+    });
+    it('fails for nobodies', async () => {
+	for (const nobody of await data.getNobodies()) {	
+	    const token = await tokenFor(nobody);
+	    const response = await request(app)
+		  .get(`/api/v1/notes`)
+		  .set('Authorization', `Bearer ${token}`)
+		  .set('Accept', 'application/json')
+		  .expect(403);
+	}
+    });
 });
