6907a18341887870983c28bdef328ac5ff2caf54
Warren MacEvoy <wmacevoy@gmail.com>
2023-10-04 19:06:38 -0600

    firebase emulator cannot write

--- my_notes_app/src/db.js
+++ my_notes_app/src/db.js
@@ -1,35 +1,47 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import 'firebase/auth';
-import { collection, doc, setDoc, getFirestore, serverTimestamp } from "firebase/firestore";
+// thanks to https://www.doc.ic.ac.uk/~nuric/coding/how-to-setup-and-connect-firebase-emulators.html
+
+import { initializeApp, FirebaseOptions } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { collection, doc, setDoc, getFirestore, serverTimestamp, connectFirestoreEmulator }
+  from "firebase/firestore";
+// import { connectStorageEmulator, getStorage } from "firebase/storage";
+import { connectAuthEmulator, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 import { FirebaseEnv } from './FirebaseEnv';
 
 let app = null;
-let firestore = null;
 
-const config = async (firestore,options) => {
+const config = async (app,options) => {
+    if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
+	const dbHost = `${process.env.REACT_APP_NAME}-${process.env.REACT_APP_NODE_ENV}-db`;
+	console.log(`connecting to firebase emulator on ${dbHost}`);
+	connectAuthEmulator(getAuth(app), `http://${dbHost}:${process.env.REACT_APP_AUTH_PORT}`);
+	// connectFirestoreEmulator(getFirestore(app), `http://${dbHost}:${process.env.REACT_APP_FIRESTORE_PORT}`);
+	// connectStorageEmulator(getStorage(app), "localhost", 9199);
+    }
     if (options !== undefined && options.user !== undefined) {
 	const user = options.user;
-	await signInWithEmailAndPassword ( getAuth(), user.email, user.password );
+	await signInWithEmailAndPassword ( getAuth(app), user.email, user.password );
     }
-    const id = `${process.env.APP}_${process.pid}`;
+    const id = `${process.env.REACT_APP_NAME}_${process.pid}`;
     const timestamp = serverTimestamp();
-    const heartbeats = collection(firestore,'heartbeats');
+    const heartbeats = collection(getFirestore(app),'heartbeats');
     const heartbeat = doc(heartbeats,id);
     await setDoc(heartbeat,{ timestamp });
 }
 
 export const db = async (options) => {
-    if (firestore === null) {
+    if (app === null) {
 	const firebaseEnv = new FirebaseEnv(options);
 	const firebaseConfig = await firebaseEnv.getConfig();
+
 	app = initializeApp(firebaseConfig);
-	firestore = getFirestore(app);
-        await config(firestore, options);
+	
+        await config(app, options);
     } else if (options !== undefined) {
         await config(firestore, options);
     }
-    return firestore;
+    return getFirestore(app);
 }
 
 export default db;
--- my_notes_app/src/db.test.js
+++ my_notes_app/src/db.test.js
@@ -1,13 +1,43 @@
 const request = require('supertest');
-const { db, auth } = require('./db');
+const { db } = require('./db');
 const { Data } = require('./Data');
+
+const signup = async (user) => {
+    const emulating =
+	  (process.env.REACT_APP_NODE_ENV === 'development')
+	  || (process.env.REACT_APP_NODE_ENV === 'test');
+    
+    const dbHost = `${process.env.REACT_APP_NAME}-${process.env.REACT_APP_NODE_ENV}-db`;
+    const authPort = parseInt(process.env.REACT_APP_AUTH_PORT);
+    const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+    const authUrl = emulating
+	  ? `http://${dbHost}:${authPort}/identitytoolkit.googleapis.com/v1`
+	  : `https://identitytoolkit.googleapis.com/v1`;
+
+    const signupUrl = `${authUrl}/accounts:signUp`;
+
+    console.log(`url ${signupUrl}`);
+    await fetch(`${signupUrl}?key=${apiKey}`,{
+	  method: 'POST',
+	  headers: {
+	      'Accept': 'application/json',
+	      'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify({
+	      email: user.email,
+	      password: user.password
+	  })
+    });
+    return user;
+}
 
 describe('db', () => {
     it('loads in  600 seconds or less', async () => {
 	const data = new Data();
 	const admins = await data.getAdmins();
-	const user = admins[0];
-	const firebase = await db({user});
+	const admin = admins[0];
+	await signup(admin);
+	const firebase = await db({admin});
 	expect(firebase !== null).toBe(true);
     },5000);
 });
